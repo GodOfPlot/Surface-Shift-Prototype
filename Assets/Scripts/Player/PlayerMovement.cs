592c65ed8af8d9043c54d471803ab449afc1d71b
GodOfPlot <oleksandrhodorov@gmail.com>
2026-02-01 16:59:01 +0100

    Implementation of the PlayerMovement prototype is complete.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 	[Header("Jump Settings")]
 	[SerializeField] private float jumpForce = 1000f;
+	[SerializeField] private float smallJumpForce = 200f;
 	[SerializeField] private float fixationTime = 3.0f;
 
 	[Space]
@@ -13,20 +14,34 @@ public class PlayerMovement : MonoBehaviour
 
 	[Header("Input Actions")]
 	[SerializeField] private InputActionReference jump;
+	[SerializeField] private InputActionReference smallJump;
 
 	// Timers
 	private readonly Timer startedJumpTimer = new();
 	private readonly Timer groundedTimer = new();
 
+	// Surface normal
+	private Vector3 normal = Vector3.up;
+
 	// State
 	private bool startedJumpCheck = false;
 	private bool groundedJumpCheck = true;
+	private bool smallJUmpActivated = false;
 
 	// Components
 	private Rigidbody rb;
 
-	private void OnEnable() => jump.action.performed += _ => InputStartJump();
-	private void OnDisable() => jump.action.performed -= _ => InputStartJump();
+	private void OnEnable()
+	{
+		jump.action.performed += _ => InputJump();
+		smallJump.action.performed += _ => InputSmallJump();
+	}
+
+	private void OnDisable()
+	{
+		jump.action.performed -= _ => InputJump();
+		smallJump.action.performed -= _ => InputSmallJump();
+	}
 
 	private void Start() => rb = GetComponent<Rigidbody>();
 
@@ -41,21 +56,45 @@ public class PlayerMovement : MonoBehaviour
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		if (!startedJumpCheck)
-			return;
-
 		string tag = collision.collider.tag;
+		normal = collision.contacts[0].normal;
+
+		if (!startedJumpCheck || smallJUmpActivated)
+			return;
 
 		switch (tag)
 		{
 			case "Ground":
-				ChangeState(collision.transform);
+				ChangeStateToFixed(collision.transform);
 				break;
 
 			case "Wall":
-				ChangeState(collision.transform);
+				ChangeStateToFixed(collision.transform);
 				groundedTimer.Start(fixationTime);
 				break;
+		}
+	}
+
+	private void CheckSurfaceContact()
+	{
+		if (rb.isKinematic)
+			return;
+
+		groundedJumpCheck = false;
+
+		Vector3 p1 = transform.position;
+		Vector3 p2 = transform.position + Vector3.up * 1.0f;
+		float radius = 0.6f;
+
+		Collider[] hits = Physics.OverlapCapsule(p1, p2, radius);
+
+		foreach (Collider col in hits)
+		{
+			if (col.CompareTag("Ground") || col.CompareTag("Wall"))
+			{
+				groundedJumpCheck = true;
+				break;
+			}
 		}
 	}
 
@@ -63,26 +102,42 @@ public class PlayerMovement : MonoBehaviour
 	{
 		transform.SetParent(null);
 		rb.isKinematic = false;
+		groundedTimer.Stop();
 	}
 
-	private void ChangeState(Transform parent)
+	private void ChangeStateToFixed(Transform parent)
 	{
 		transform.SetParent(parent);
 		rb.isKinematic = true;
 		startedJumpCheck = false;
+		groundedJumpCheck = true;
 	}
 
-	private void InputStartJump()
+	private void InputJump()
 	{
-		if (groundedJumpCheck)
+		CheckSurfaceContact();
+		if (!groundedJumpCheck && !smallJUmpActivated)
 			return;
 
-		Unparent();
-		StartJump(Camera.main.transform.forward);
-
-		startedJumpTimer.Start(jumpAttachDelay);
+		StartJump(Camera.main.transform.forward, jumpForce);
 		groundedJumpCheck = false;
+		smallJUmpActivated = false;
 	}
 
-	private void StartJump(Vector3 force) => rb.AddForce(force * jumpForce, ForceMode.Acceleration);
+	private void InputSmallJump()
+	{
+		CheckSurfaceContact();
+		if (!groundedJumpCheck)
+			return;
+
+		StartJump(normal, smallJumpForce);
+		smallJUmpActivated = true;
+	}
+
+	private void StartJump(Vector3 force, float speed)
+	{
+		Unparent();
+		rb.AddForce(force * speed, ForceMode.Acceleration);
+		startedJumpTimer.Start(jumpAttachDelay);
+	}
 }
