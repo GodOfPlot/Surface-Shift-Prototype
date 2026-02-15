3e73c7474b500a257f88ace766a1e9c7d1c5ad58
GodOfPlot <oleksandrhodorov@gmail.com>
2026-02-15 13:15:52 +0100

    Work on the enemy is complete, work on the level has been done.

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,9 +1,12 @@
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
+[SelectionBase]
 public class EnemyController : MonoBehaviour
 {
-    private GameObject player;
+	[SerializeField] private Transform headTransform;
+
+	private GameObject player;
 	private LineRenderer lineRenderer;
 
 	private Vector3 shotVector;
@@ -14,15 +17,13 @@ public class EnemyController : MonoBehaviour
 	{
 		lineRenderer = GetComponent<LineRenderer>();
 		lineRenderer.enabled = false;
+		shotTimer.Start(0.1f);
+		shotVector = headTransform.forward;
 	}
 
 	private void Update()
 	{
-		if (shotTimer.Update(Time.deltaTime))
-		{
-			Shot();
-			shotTimer.Start(1f);
-		}
+		Shot();
 	}
 
 	#region Shot
@@ -36,10 +37,7 @@ public class EnemyController : MonoBehaviour
 	private void OnTriggerExit(Collider other)
 	{
 		if (other.CompareTag("Player"))
-		{
-			shotVector = transform.forward;
 			player = null;
-		}
 	}
 
 	private void Shot()
@@ -49,29 +47,26 @@ public class EnemyController : MonoBehaviour
 
 		Vector3 to = VectorToPlayer(transform.position, player.transform.position);
 
-		float rotateSpeed = 0.1f;
+		float rotateSpeed = Time.deltaTime * 0.5f;
 		shotVector = Vector3.RotateTowards(shotVector, to, rotateSpeed, 0f);
-			
-		if (30 > Vector3.Angle(shotVector, to))
-			RayShot(shotVector);
+		headTransform.rotation = Quaternion.LookRotation(shotVector);
 
+		if (60 > Vector3.Angle(shotVector, to))
+			RayShot(shotVector);
 	}
 
 	private async void RayShot(Vector3 vector)
 	{
 		float maxDistance = 50f;
-		Ray ray = new Ray(transform.position, vector);
-		RaycastHit hit;
+		Ray ray = new(transform.position, vector);
 
 		Vector3 endPoint = transform.position + vector * maxDistance;
 
-		if (Physics.Raycast(ray, out hit, maxDistance))
+		if (Physics.Raycast(ray, out RaycastHit hit, maxDistance))
 		{
 			endPoint = hit.point;
 			if (hit.collider.CompareTag("Player"))
-			{
 				Debug.Log("Player hit!");
-			}
 		}
 
 		lineRenderer.SetPosition(0, transform.position);
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[SelectionBase]
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
